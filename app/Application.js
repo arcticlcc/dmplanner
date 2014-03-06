f5933250af60c59618a0e4175626135c49ec9009
Josh Bradley <jbradley@arcticlcc.org>
2014-03-05 19:01:50 -0900

    Add basic error notifications and template creation to Application.

--- app/Application.js
+++ app/Application.js
@@ -11,6 +11,8 @@ Ext.define('DMPlanner.Application', {
 
     extend: 'Ext.app.Application',
 
+    requires:['Ext.ux.window.Notification', 'DMPlanner.util.UUID'],
+
     views: [
     // TODO: add views here
     ],
@@ -25,5 +27,92 @@ Ext.define('DMPlanner.Application', {
     stores: [
     // TODO: add stores here
         'Plans'//
-    ]
+    ],
+
+    launch: function() {
+        //Use Notifications for Ext errors
+        Ext.Error.handle = function(err) {
+            DMPlanner.app.showError(err.msg);
+        };
+
+        //get plan template
+        Ext.Ajax.request({
+            url: 'data.json',
+            success: function(response, opts) {
+                var obj = Ext.decode(response.responseText);
+
+                Ext.define('DMPlanner.data.PlanTemplate', {
+                    singleton: true,
+
+                    plans: obj
+                });
+
+            },
+            failure: function(response, opts) {
+                var err = 'Server-side failure with status code ' + response.status;
+                DMPlanner.app.showError(err);
+            }
+        });
+    },
+
+    /**
+     * Store the last application error.
+     * @param {String} txt The error string.
+     */
+    setError: function(txt) {
+        this.lastError = txt;
+    },
+
+    /**
+     * Retrieve the last application error.
+     * @return {String} The last recorded error.
+     */
+    getError: function() {
+        return this.lastError;
+    },
+
+    /**
+     * Creates an error notification.
+     * @param {String} txt The error string.
+     */
+    showError: function(txt) {
+        Ext.create('widget.uxNotification', {
+            title: 'Error',
+            glyph: 'xf12a@FontAwesome',
+            html: txt
+        }).show();
+    },
+
+    /**
+     * Creates a DMP template.
+     * Assigns UUIDs to ensure uniqueness.
+     * @param {Object} plan The plan template to use.
+     */
+    getPlanTemplate: function(plan) {
+        var uuid = DMPlanner.util.UUID.uuid;
+
+        //assign plan id
+        plan.id = uuid();
+        //loop sections
+        Ext.each(plan.sections, function(section) {
+            section.id = uuid();
+            section.plan_id = plan.id;
+
+            Ext.each(section.groups, function(group) {
+                group.id = uuid();
+                group.plan_id = plan.id;
+                group.section_id = section.id;
+
+                Ext.each(section.questions, function(question) {
+                    question.id = uuid();
+                    question.plan_id = plan.id;
+                    question.section_id = section.id;
+                    question.group_id = group.id;
+                });
+            });
+
+        });
+
+        return plan;
+    }
 });
