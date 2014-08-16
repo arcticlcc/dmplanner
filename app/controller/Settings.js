85af45c0e32686d5aba5d4f02bf9f55513c01b5e
jlblcc <jbradley@arcticlcc.org>
2014-08-15 19:23:37 -0800

    Add plan level button to sectionlist titlebar.

--- app/controller/Settings.js
+++ app/controller/Settings.js
@@ -6,12 +6,18 @@ Ext.define('DMPlanner.controller.Settings', {
     refs: [{
         ref: 'combo',
         selector: 'dmpsettings #levelCombo'
+    }, {
+        ref: 'button',
+        selector: 'sectionlist #levelButton'
     }],
 
     init: function(application) {
         this.control({
             'dmpsettings #levelCombo': {
-                select: this.updateLevel
+                select: this.onLevelComboSelect
+            },
+            'sectionlist #levelButton': {
+                change: this.onLevelButtonChange
             }
         });
 
@@ -32,17 +38,39 @@ Ext.define('DMPlanner.controller.Settings', {
     onSelectPlan: function(record) {
         var levels = [], //
             combo = this.getCombo(),//
+            button = this.getButton(), //
+            val = record.get('defaultLevel') || 0, //
             data = record.data.levels || DMPlanner.data.PlanTemplate.levels;
 
+        button.menu.removeAll();
+
+        //setup combo and button
         Ext.each(data, function(l, i) {
             levels.push([i, l]);
+            button.menu.add({
+                group        : button.id,
+                itemIndex    : i,
+                checkHandler : button.checkHandler,
+                scope        : button,
+                checked      : i === val,
+                xtype: 'menucheckitem',
+                text: l,
+                level: i
+            });
         });
 
         combo.getStore().loadData(levels);
         combo.setDisabled(!levels.length);
-        combo.setValue(record.get('defaultLevel') || 0);
+        combo.setValue(val);
 
-        this.updateLevel(combo, [combo.findRecordByValue(combo.getValue())]);
+
+        button.setActiveItem(button.menu.items.findBy(function(i) {
+            return i.checked;
+        }));
+
+        button.setVisible(!!levels.length);
+
+        this.updateLevel(combo.getValue());
     },
 
     /**
@@ -50,8 +78,39 @@ Ext.define('DMPlanner.controller.Settings', {
      * @param {Ext.form.field.ComboBox} combo This combo box
      * @param {Array} records The selected records
      */
-    updateLevel: function(combo, records) {
-        var val = records ? records[0].get('field1') : 0;
+    onLevelComboSelect: function(combo, records) {
+        var val = records ? records[0].get('field1') : 0,
+            btn = this.getButton();
+
+        this.updateLevel(val);
+
+        if(val !== btn.getActiveItem().level) {
+            btn.setActiveItem(btn.menu.items.findBy(function(i) {
+                return i.level === val;
+            }), true);
+        }
+    },
+
+    /**
+     * Fired when the level button is changed.
+     * @param {Ext.button.Cycle} btn This button
+     * @param {Ext.menu.Item} itm The selected menu item
+     */
+    onLevelButtonChange: function(btn, itm) {
+        var val = itm.level ||  0,
+            combo = this.getCombo();
+
+        this.updateLevel(val);
+
+        combo.setValue(itm.level);
+    },
+
+    /**
+     * Set the plan level..
+     * @param {Integer} level The level
+     */
+    updateLevel: function(level) {
+        var val = level ? level : 0;
 
         DMPlanner.util.LevelFilter.value = val;
         this.fireEvent('changelevel', val);
--- app/view/SectionList.js
+++ app/view/SectionList.js
@@ -1,12 +1,30 @@
 Ext.define("DMPlanner.view.SectionList", {
     extend : 'Ext.grid.Panel',
     alias : 'widget.sectionlist',
+    requires : ['Ext.button.Cycle'],
+
 
     title : 'Sections',
     columnLines : false,
     hideHeaders : true,
     store : null,//'Sections',
     cls : 'sectionList',
+    header:{
+        titlePosition: 0,
+        items:[{
+            itemId: 'levelButton',
+            xtype:'cycle',
+            showText: true,
+            prependText: 'Plan Level: ',
+            menu: {
+                items:[{
+                    text: 'Default',
+                    level: 0
+                }]
+            },
+            hidden: true
+        }]
+    },
     tools : [{
         type : 'home',
         tooltip : 'Home',
